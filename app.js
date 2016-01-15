d92a64d54785df6555d3e1820bd02c52c1edcf9a
sim <simeonbourne@gmail.com>
2016-01-14 20:27:37 -0400

    Add buttons and functionality

--- app.js
+++ app.js
@@ -8,16 +8,50 @@ $(document).ready(
 
             var amount = $('input[type=number]').val();
 
+
             if ( item === "" ){
                 alert("Your item is empty");
             } else if (amount === ""){
                 alert("You need an amount");
             } else {
-                $("ul.items").append("<li>"+ item + " (" + amount + ")" + "<a>Delete</a>") + "</li>");
-                $("ul.items li").html("<a>Delete</a>");
-                $("input").val("");
-               
+                //Create list elements and buttons with specific ids
+
+                $("ul.items").append("<li> <span class='edit'>"+ item + "</span> (" + amount + ") " +
+                "<button class = 'dl'>Delete</button> | <button class ='md'>Mark Done</button>"+
+                 " | <button class = 'ed'>Edit</button></li>");
+
+
+
+                $("input").val(""); //Clear input after submit  
+
+                
+                //Strike completed items
+                $("button.md").on('click',function(){
+                    $(this).parent().addClass("strike");
+                    $(this).siblings().addClass("strike");
+                    $(this).addClass("strike");
+                    $(this).parent().css({"background-color":"green", "color":"#FFFFFF"});
+                    
+                }) 
+
+
+                //Delete an item from the list  
+                $("button.dl").on('click',function(){
+                    $(this).parent().remove();//empty removes children as well
+
+                })      
+
+                 //Edit an item
+                 $("button.ed").on('click',function(){
+                    var text = prompt("Place edited text here:");
+                    //$(this).parent().text(text);
+                    $(this).siblings(".edit").text(text);
+
+                 }) 
+
             }
+
+
         });
         
         
